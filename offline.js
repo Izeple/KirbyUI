67914ea5bb9d09c764c4bd9786e854b4cff900b2
Izeple <izeple@hotmail.co.th>
2020-07-11 17:57:56 +0700

    ver1.00

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1594464327,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/112x-sheet0.png",
+		"images/112x-sheet1.png",
+		"images/112x-sheet2.png",
+		"images/decrease-sheet0.png",
+		"images/tiledbackground.png",
+		"images/kirby-sheet0.png",
+		"images/kirby-sheet1.png",
+		"images/return-sheet0.png",
+		"images/untitled-sheet0.png",
+		"images/rr_dirac-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
